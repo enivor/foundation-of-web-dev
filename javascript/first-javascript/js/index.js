9e7490c6f2491b08f06001e76bd3f9ba3ee7ab07
enivor <enivor1126@gmal.com>
2022-06-20 12:49:59 -0500

    Day 1 Javascript Practice

---
+++ javascript/first-javascript/js/index.js
@@ -0,0 +1,11 @@
+var total = 0;
+total = total +2;
+total += 3;
+total++;
+console.log("Total is " + total);
+
+/* 
+this is a multiline comment in javascript
+*/
+
+// this is a single line commet in javascript
